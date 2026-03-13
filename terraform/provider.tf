9276fe0dbd9ec7c9ff0af7dbbdb551aa1fc0d68d
marcelle.tabosa <marcelle.tabosa.97@gmail.com>
2026-03-13 20:20:17 -0300

    fix iac

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.0"
 
+  backend "s3" {
+    bucket = "fiapx-video-terraform-state"
+    key    = "infra/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
